241f7f699995a2a85a27c6084ed24b5d0a21c2d2
leejunsu <qkfhtit@gmail.com>
2021-11-25 14:45:52 +0900

    뷰 페이저에서 프래그먼트 사용

---
+++ README.md
@@ -0,0 +1,1 @@
+# and.kotlin.viewPager
---
+++ app/src/main/java/com/ljs/and/viewpager/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ljs.and.viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.ljs.and.viewpager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val fragmentList = listOf(FragmentA(), FragmentB(), FragmentC(), FragmentD())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        binding.viewPager.adapter = adapter
+
+        val tabTitles = listOf<String>("A", "B", "C", "D")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
+
+    }
+}
